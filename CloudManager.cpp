b5e41f5a6e3475255e1c3aa72fd6b421d824b8ac
thehilmisu <ozcanhakanzi@gmail.com>
2024-10-02 17:16:28 +0200

    git ignore file added

---
+++ CloudManager.cpp
@@ -0,0 +1,5 @@
+//
+// Created by developer on 10/2/24.
+//
+
+#include "CloudManager.h"
---
+++ CloudManager.h
@@ -0,0 +1,16 @@
+//
+// Created by developer on 10/2/24.
+//
+
+#ifndef CLOUDMANAGER_H
+#define CLOUDMANAGER_H
+
+
+
+class CloudManager {
+
+};
+
+
+
+#endif //CLOUDMANAGER_H
